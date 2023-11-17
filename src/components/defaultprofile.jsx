33c53806bb9dfcef8e29921f17ab28200760f6c7
vaidik-bajpai <115713002+vaidik-bajpai@users.noreply.github.com>
2023-11-17 17:27:41 +0530

    First Commit

---
+++ src/components/defaultprofile.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+const DefaultProfile = () => {
+    return(
+        <div>
+            <h3>Huh oh nothing to see here!</h3>
+        </div>
+    )
+}
+
+export default DefaultProfile
